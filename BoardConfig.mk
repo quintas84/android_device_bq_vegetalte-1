8416b31bf7d23fba06e4ea4823a15dc0d3102e51
paloda <paloda09@gmail.com>
2016-09-18 20:00:39 +0200

    osprey: Implement Custom PowerHAL
    The main purpose here is to provide configurable power
    profiles. The HAL configures behaviour of interactive
    governor according to the power profile selected.
    
    - Powersave profile: the CPU frequency is limited to 998MHz and the CPU
      is not boosted on user interaction. This will save some juice without
      making the phone slow.
    - Performance profile: the CPU is constantly boosted.
    - Balanced profile: the CPU is boosted on user interaction.
      When the screen is turned off, the governor is tuned to lower the
      power consumption to save power in case of long lasting screen off
      activities such as music playback.
    - Powersave_bias profile: AKA Efficiency profile.This profile is same as
      powersave profile except for the max CPU frequency which is not limited.
    
    With this PowerHAL cpu-boost is not needed, so disable it.
    
    Change-Id: I57b02100c891cb0808ad045bc3b4f8eb33031194
    
    (cherry picked from commit 01d862fe93141e44c4bc061f1ddfa6ff980ad621)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -21,7 +21,7 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno306
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/bq/msm8939
-TARGET_KERNEL_CONFIG := cyanogenmod_vegetalte_defconfig
+TARGET_KERNEL_CONFIG := vegetalte_defconfig
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := vegetalte,Aquaris_E5
