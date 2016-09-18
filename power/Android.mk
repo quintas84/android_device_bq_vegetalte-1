f51cae498aa66466f0f1ca6101457d9030a8a132
paloda <paloda09@gmail.com>
2016-09-18 22:38:00 +0200

    fix name power_8939.c
    Bug: 27875275
    Change-Id: Ibf5f749c0d3d421128237915542a52f941574ac9
    
    (cherry picked from commit e4802963084a9b42b6d91f7811d2a562e72e7487)
    
    (cherry picked from commit 30500cab43a9f93f80585ef4d8ca1e75db71cb73)
    
    (cherry picked from commit 06e409a73ef8a73375b4aaa81e81114e4e12d48b)

--- power/Android.mk
+++ power/Android.mk
@@ -18,7 +18,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := $(TARGET_POWERHAL_HEADER_PATH)
-ifeq ($(filter lux merlin,$(TARGET_DEVICE)),)
+ifeq ($(filter vegetalte,$(TARGET_DEVICE)),)
 LOCAL_SRC_FILES := power.c
 else
 LOCAL_SRC_FILES := power_8939.c
