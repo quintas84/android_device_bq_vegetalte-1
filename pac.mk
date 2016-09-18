57823dc5e41a322a2723e07922c1f9eeb9a22b38
paloda <paloda09@gmail.com>
2016-09-18 22:50:11 +0200

    pacify

--- pac.mk
+++ pac.mk
@@ -3,13 +3,13 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/bq/vegetalte/full_vegetalte.mk)
 
 PRODUCT_RELEASE_NAME := Aquaris E5
-PRODUCT_NAME := cm_vegetalte
+PRODUCT_NAME := pac_vegetalte
 
 # Set product device & name
 PRODUCT_BUILD_PROP_OVERRIDES += \
